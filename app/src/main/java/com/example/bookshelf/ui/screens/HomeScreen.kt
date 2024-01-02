ca7926600cb9a78078d85bd9482327b80c3a986a
Laura Voetzsch <56238822+Laurax64@users.noreply.github.com>
2024-01-02 18:30:11 +0100

    Implement basic vertical grid

--- app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
@@ -4,25 +4,18 @@ package com.example.bookshelf.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -79,47 +72,43 @@ fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
 
 @Composable
 fun BooksList(books: Books, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
+    LazyVerticalGrid(GridCells.Adaptive(180.dp), modifier = modifier
+    ){
         items(items = books.items) { item ->
-            BookCard(
-                item,
-                modifier = modifier
-                    .padding(4.dp)
-                    .fillMaxWidth()
-                    .aspectRatio(1.5f)
-            )
+            BookCard(item, modifier = modifier)
         }
     }
 }
 
+/**
+ * Displays a card with the books thumbnail, title and authors
+ */
 @Composable
 fun BookCard(item: Item, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier,
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-    ) {
-        Row() {
-            AsyncImage(
-                model = ImageRequest.Builder(context = LocalContext.current)
-                    .data(item.volumeInfo.imageLinks.thumbnail
-                        .replace("http", "https"))
-                    .crossfade(true).build(),
-                error = painterResource(R.drawable.ic_broken_image),
-                placeholder = painterResource(R.drawable.loading_img),
-                modifier = Modifier
-                    .clip(RoundedCornerShape(50.dp))
-                    .size(90.dp),
-                contentDescription = (item.volumeInfo.title),
-                contentScale = ContentScale.Crop
+   /**
+            Text(
+                text = item.volumeInfo.title,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
             )
-            Column(modifier = Modifier.padding(1.dp)) {
-                Text(item.volumeInfo.title, style = MaterialTheme.typography.headlineSmall)
-                Text(item.volumeInfo.authors.toString())
-            }
-        }
-    }
+            **/
+            AsyncImage(
+                modifier = modifier.size(width = 120.dp, height = 200.dp),
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(item.volumeInfo.imageLinks.thumbnail.
+                    replace("http", "https"))
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                error = painterResource(id = R.drawable.ic_broken_image),
+                placeholder = painterResource(id = R.drawable.loading_img)
+            )
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
